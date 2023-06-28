06003ae55ca26f1b55f443df0fc824aa680bc337
Hendrix <drixyhendrix@yahoo.com>
2023-06-27 21:30:22 -0500

    terraorm-rev

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "profile" {
+  type    = string
+  default = "default"
+}
+
+variable "topic_name" {
+  default = "sns-Dev"
+}
+
+variable "Team" {
+  default = "Dev"
+}
+
+variable "protocol_type" {
+  default = "email"
+}
+
+variable "endpoint" {
+  default = "boss@gmail.com"
+}
